a518a391e1f694d493b2b96d700bae6149477f3f
Leonardo Ferreira <leualemax@gmail.com>
2020-05-26 09:01:48 -0300

    send buiulded content to gh pages

---
+++ precache-manifest.73fc20f95ea4550c09aa1a3fe4be70d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5c324426880875a48993dc13735969e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "617f61880a3f17d0f5bf",
+    "url": "/static/css/main.6c7a5eb7.chunk.css"
+  },
+  {
+    "revision": "8026db3ed2e9796adb98",
+    "url": "/static/js/2.3d774afc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3d774afc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "617f61880a3f17d0f5bf",
+    "url": "/static/js/main.40e63bd5.chunk.js"
+  },
+  {
+    "revision": "f7c2ab26754a10b02d0e",
+    "url": "/static/js/runtime-main.5f294fbd.js"
+  }
+]);
